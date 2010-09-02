a2d0e8f54f8b5af912cc49a876fb442d60c37372
David Jones <davidjones@gamestop.com>
2010-09-02 13:34:35 -0500

    First Commit!

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery
+
+
+  private
+
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == 'david' && password == 'jones'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '48fd68a28e0b92ab7da855a1d67cea8d9f77e19154806f161274e908625f7fb63edf0fcaa3abd59d0cd54a97cbf76d7470e2d63632f25b3e32645f9675ea29b2'
